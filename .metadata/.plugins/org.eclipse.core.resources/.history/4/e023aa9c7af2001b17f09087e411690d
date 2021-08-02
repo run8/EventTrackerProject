db6e1c2168fa776d617fa03ecb4a40cf99981011
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-08-02 10:50:39 -0700

    Configured project to deploy to AWS

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4/e023aa9c7af2001b17f09087e411690d
@@ -0,0 +1,11 @@
+package com.skilldistillery.flighttracker.services;
+
+import java.util.List;
+
+import com.skilldistillery.flighttracker.entities.Flight;
+
+public interface FlightService {
+	
+	List<Flight> allFlights();
+
+}
--- FlightTracker/src/main/java/com/skilldistillery/flighttracker/FlightTrackerApplication.java
+++ FlightTracker/src/main/java/com/skilldistillery/flighttracker/FlightTrackerApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.flighttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlightTrackerApplication {
+public class FlightTrackerApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FlightTrackerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightTrackerApplication.class, args);
